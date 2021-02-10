f928d2e086c6414d3b814fa664a5b77752fe0368
Marcin Lentner <marcin_len@yahoo.co.uk>
2021-02-10 20:30:28 +0000

    Add Router

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const Koa = require("koa");
+const KoaRouter = require("koa-router");
 
 const app = new Koa();
+const router = new KoaRouter();
 
-app.use(async (ctx) => (ctx.body = "Hello, World!"));
+// create new route
+router.get("/about", (ctx) => (ctx.body = "About Page"));
+
+// Router Middleware
+app.use(router.routes());
 
 app.listen(3005, () => console.log("Started Koa..."));
